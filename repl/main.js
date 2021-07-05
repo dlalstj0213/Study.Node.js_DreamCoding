2eca929ce73aa8d0856fb6f55ba31198f96bf6f9
dlalstj0213 <dlalstj0213@gmail.com>
2021-07-05 20:33:10 +0900

    add REPL

---
+++ repl/main.js
@@ -0,0 +1,15 @@
+console.log('Hello World');
+
+/**
+ * REPL?
+ * - Read Eval Print Loop (REPL)
+ * - R -> E -> P -> L -> R -> E -> ...
+ *
+ * Terminal 실행 방법
+ * 1. 현재 경로로 터미널 접근
+ * $ cd reple
+ * 2. node [실행할 js 파일 / 실행할 js 파일.js]
+ * $ node main
+ * $ node main.js
+ * 3. 실행 완료
+ */
